baa593844b241a94d4fe21b0dad62ab1b4c9cff1
savvas1997 <smeletoglouaa5@gmail.com>
2022-08-20 15:47:52 +0300

    commit

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,37 +5,21 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\MultiImg;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\SubSubCategory;
+
 class ProductController extends Controller
 {
     //
-    protected $fillable = [
-        'brand_id',
-        'category_id',
-        'subcategory_id',
-        'subsubcategory_id',
-        'product_name_en',
-        'product_name_gr',
-        'product_slug_en',
-        'product_slug_gr',
-        'product_code',
-        'product_qty',
-        'product_tags_en',
-        'product_tags_gr',
-        'product_size_en',
-        'product_size_gr',
-        'product_color_en',
-        'product_color_gr',
-        'selling_price',
-        'discount_price',
-        'short_descp_en',
-        'short_descp_gr',
-        'long_descp_en',
-        'long_descp_gr',
-        'product_thambnail',
-        'hot_deals',
-        'featured',
-        'special_offer',
-        'special_deals',
-        'status',
-    ];
+   public function addproduct(){
+
+        $categories = Category::latest()->get();
+        $brands = Brand::latest()->get();
+
+        return view('backend.product.product_add',compact('categories','brands'));
+
+   }
 }
